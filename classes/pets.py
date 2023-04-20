60fd69bb2c1552b0fdc5658c178bb18227e15ae5
Yevhen Horoshko <degycool@gmail.com>
2023-04-20 03:42:57 +0300

    feat(classes): add new classes functonality

---
+++ classes/pets.py
@@ -0,0 +1,10 @@
+ #!/usr/bin/env python3
+
+"""Script contains a list of useful functions for work with dictionaries.
+
+Functions:
+- union, intersection, difference, symmetric difference
+- sort dictionary by keys/values in different orders
+- swap dictionary
+"""
+
